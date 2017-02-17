c4b1afccd18ea27336ac3f50e58e72250f644d85
Biridiana Angeles <biritaangeles@gmail.com>
2017-02-17 13:34:25 -0600

    Rama creada por Biri

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel--Biritha Angeles
                 </div>
 
                 <div class="links">
